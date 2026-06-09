3892da4346cd42914f8c114c0605a2ae41dab63d
Patricio Gonzalez <eleinssystems@gmail.cl>
2026-06-09 15:27:19 -0400

    instagram link

--- app/page.tsx
+++ app/page.tsx
@@ -40,13 +40,15 @@ export default function Home() {
       >
         Sitio Web
       </a>
-<a
-  href="https://instagram.com/eleinssystems"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Instagram
-</a>
+
+      <a
+        href="https://www.instagram.com/eleinssystems?igsh=MnJzdnE1ZWpodmoy"
+        target="_blank"
+        className="bg-pink-500 text-white px-6 py-3 rounded-2xl font-bold"
+      >
+        Instagram
+      </a>
+
       <a
         href="https://wa.me/56984370601"
         target="_blank"
