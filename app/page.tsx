58ee3fe1d6df2100b18d48fee8a562d626cc6d99
Patricio Gonzalez <eleinssystems@gmail.cl>
2026-06-09 15:14:28 -0400

    fix email size

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
       </a>
 
       <a
-        href="https://eleinssystemsinstagram.com/"
+        href="https://eleinssystems/instagram.com/"
         target="_blank"
         className="bg-pink-500 text-white px-6 py-3 rounded-2xl font-bold"
       >
